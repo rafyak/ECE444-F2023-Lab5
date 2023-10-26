d91ab82a0d79aa7bc3b478d06bb0e6af16734a28
Rafya Khan <rafya.khan@mail.utoronto.ca>
2023-10-26 10:27:24 -0400

    Add view for adding entries

--- project/app.py
+++ project/app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, g, render_template, request, session, flash, redirect, url_for
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 
 # configuration
 DATABASE = "flaskr.db"
@@ -50,6 +50,20 @@ def index():
     cur = db.execute('select * from entries order by id desc')
     entries = cur.fetchall()
     return render_template('index.html', entries=entries)
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'insert into entries (title, text) values (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
