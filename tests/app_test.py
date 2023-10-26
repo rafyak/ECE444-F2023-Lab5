83319595a1c8ea89962c4d484867bd45d0268ba9
Rafya Khan <rafya.khan@mail.utoronto.ca>
2023-10-26 13:19:27 -0400

    linting and code formatting (flake8, black)

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,9 +1,9 @@
-import os
 import pytest
 import json
 from pathlib import Path
 
 from project.app import app, db
+
 TEST_DB = "test.db"
 
 
@@ -18,6 +18,7 @@ def client():
         db.create_all()  # setup
         yield app.test_client()  # tests run here
         db.drop_all()  # teardown
+
 
 def login(client, username, password):
     """Login helper function"""
@@ -73,6 +74,7 @@ def test_messages(client):
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt;" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
+
 
 def test_delete_message(client):
     """Ensure the messages are being deleted"""
